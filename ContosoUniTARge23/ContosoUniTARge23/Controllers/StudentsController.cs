e976a698c747ed6fca888190d1c656744ab32381
MarcusPirita <piritamarcus@gmail.com>
2024-09-10 11:42:32 +0300

    10.09.24

--- ContosoUniTARge23/ContosoUniTARge23/Controllers/StudentsController.cs
+++ ContosoUniTARge23/ContosoUniTARge23/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using ContosoUniTARge23.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
 
 namespace ContosoUniTARge23.Controllers
 {
@@ -17,12 +18,56 @@ namespace ContosoUniTARge23.Controllers
                 _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(
+            string sortOrder, 
+            string currentFilter,
+            string searchString,
+            int? pageNumber)
         {
-            var result = await _context
-                .Students.ToListAsync();
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
 
-            return View(result);
+            if (searchString !=null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+            var students = from s in _context.Students
+                           select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString)
+                                    || s.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+
+                case "Date":
+					students = students.OrderByDescending(s => s.EnrollmentDate);
+					break;
+
+                case "date_desc":
+					students = students.OrderByDescending(s => s.EnrollmentDate);
+					break;
+				default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await PaginatedList<Student>
+                .CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         public async Task<IActionResult> Details(int? id)
